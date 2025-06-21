c2cf408db5ea9d7a554360983c73a5bb2a3efb8b
intel <intel@DESKTOP-TUPN40M>
2025-06-20 18:55:09 -0700

    day5 program

--- C2TC_JavaProject/src/com/tnsif/dayfive/association/isa/Manager.java
+++ C2TC_JavaProject/src/com/tnsif/dayfive/association/isa/Manager.java
@@ -1,10 +1,33 @@
 package com.tnsif.dayfive.association.isa;
 
-public class Manager {
+//Subclass inheriting from Employee
+public class Manager extends Employee {
+	private int teamSize;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	// constructors
+	public Manager() {
+		super();
 
+	}
+
+	public Manager(String name, int employeeId, String department, int teamSize) {
+		super(name, employeeId, department);
+		this.teamSize = teamSize;
+	}
+
+	// getter/setter methods
+	public int getTeamSize() {
+		return teamSize;
+	}
+
+	public void setTeamSize(int teamSize) {
+		this.teamSize = teamSize;
+	}
+
+	@Override
+	public String toString() {
+		return "Manager [TeamSize=" + teamSize + ", Name=" + getName() + ", EmployeeId=" + getEmployeeId()
+				+ ", Department=" + getDepartment() + "]";
 	}
 
 }
