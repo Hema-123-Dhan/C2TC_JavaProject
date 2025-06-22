eb174d53cb92fac75a779db39ec124d3fb763885
intel <intel@DESKTOP-TUPN40M>
2025-06-21 17:39:39 -0700

    day6 program

--- C2TC_JavaProject/src/com/tnsif/daysix/usingfinal/FinalClass.java
+++
@@ -1,8 +0,0 @@
-//Program to demonstrate final class
-package com.tnsif.daysix.usingfinal;
-
-final class FinalClass {
-	void show() {
-		System.out.println("Final class cannot be inherited");
-	}
-}
